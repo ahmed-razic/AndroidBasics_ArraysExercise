91f6c42e92a8bedaa1485aa6e134d1c74e3a499e
ahmed-razic <ahmed.razic@gmail.com>
2021-01-27 14:22:26 +0100

    added logs to logcat

--- app/src/main/java/com/example/android/androidbasics_arrayandarraylist/MainActivity.java
+++ app/src/main/java/com/example/android/androidbasics_arrayandarraylist/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.androidbasics_arrayandarraylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 
@@ -19,7 +21,35 @@ public class MainActivity extends AppCompatActivity {
         for( int i = 0; i < 10; i++) {
             intArray[i] = i;
         }
-
         Toast.makeText(this, String.valueOf(intArray[5]), Toast.LENGTH_SHORT).show();
+
+        doArrays();
+    }
+
+    public void doArrays() {
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.i("Main Activity", "Value on index 0: " + words[0]);
+        Log.v("Main Activity", "Value on index 1: " + words[1]);
+        Log.d("Main Activity", "Value on index 2: " + words[2]);
+        Log.w("Main Activity", "Value on index 3: " + words[3]);
+        Log.e("Main Activity", "Value on index 4: " + words[4]);
+        Log.i("Main Activity", "Value on index 5: " + words[5]);
+        Log.v("Main Activity", "Value on index 6: " + words[6]);
+        Log.d("Main Activity", "Value on index 7: " + words[7]);
+        Log.w("Main Activity", "Value on index 8: " + words[8]);
+        Log.e("Main Activity", "Value on index 9: " + words[9]);
+
+
     }
 }
